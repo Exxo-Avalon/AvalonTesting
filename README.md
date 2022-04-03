ee3d25001221eb18cda4cc70528b45a65cbad7b5
ghasttear1 <git@johntboyer.com>
2022-04-03 11:11:22 +1000

    commit with some basics files taken over

---
+++ README.md
@@ -0,0 +1,52 @@
+# Exxo Avalon Origins
+
+Exxo Avalon Origins is the tModLoader reimplementation of the tConfig Terraria Avalon mod.
+
+Stay updated and join the discord community: <https://discord.gg/rtm99Uq>
+
+## Todo List
+
+### General
+
+* Update to-do list
+
+### Pre-Release
+
+* Fix even more consumables
+* Fix more buffs
+* Do late-game world gen
+* Make sure some less major stuff works
+* Do even more World Gen
+* Add NPC Gore
+* Do many minor (usually decorative) tile things
+* Fix a few NPC AIs
+* Implement Dusts
+* Rewrite Team Mirror
+* Fit Avalon progression within the 1.3.x progression
+* Fix more bugs
+* Add missing stuff
+* Fit expert mode items (and maybe AIs) in and disable expert mode
+* Do Primordial Ore
+* Fix Oblivion
+* FIX ISSUES
+* Remove items that were added in vanilla 1.3
+
+### Extra
+
+* Redo Desert Beak and Bacterium Prime
+* Get many sprites updated
+* Add better boss health bars (using event progress bars)
+* Add Ultrablivion and other TConfig only stuff
+* Dye Slimes
+* Add Death point mirror
+* Clean up code
+* Add more content
+* Cross mod compatibility
+* Potion campfires
+* Moar campfires, heart lanterns and such
+* Add and implement Contagion
+* Add the Tropics
+
+### Easier Recipes
+
+* Chlorophyte armor and weapons
---
+++ Version.cs
@@ -0,0 +1,77 @@
+﻿namespace ExxoAvalonOrigins;
+
+public class Version
+{
+    private readonly int rewrite;
+    private readonly int major;
+    private readonly int minor;
+    private readonly int fix;
+    private readonly bool isDev;
+
+    public Version(int rewrite, int major, int minor, int fix, bool isDev = false)
+    {
+        this.rewrite = rewrite;
+        this.major = major;
+        this.minor = minor;
+        this.fix = fix;
+        this.isDev = isDev;
+    }
+    
+    public Version(string version)
+    {
+        string[] components = version.Split('.');
+        rewrite = int.Parse(components[0]);
+        major = int.Parse(components[1]);
+        minor = int.Parse(components[2]);
+        fix = int.Parse(components[3]);
+        if (components.Length > 4)
+        {
+            isDev = components[4] == "dev";
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{rewrite}.{major}.{minor}.{fix}{(isDev ? ".dev" : "")}";
+    }
+
+    public static bool operator <(Version v1, Version v2)
+    {
+        if (v2 == null)
+        {
+            return false;
+        }
+
+        if (v1 == null)
+        {
+            return true;
+        }
+
+        if (v1.rewrite != v2.rewrite)
+        {
+            return v1.rewrite < v2.rewrite;
+        }
+
+        if (v1.major != v2.major)
+        {
+            return v1.major < v2.major;
+        }
+
+        if (v1.minor != v2.minor)
+        {
+            return v1.minor < v2.minor;
+        }
+
+        if (v1.fix != v2.fix)
+        {
+            return v1.fix < v2.fix;
+        }
+
+        return v1.isDev;
+    }
+
+    public static bool operator >(Version v2, Version v1)
+    {
+        return v1 < v2;
+    }
+}
