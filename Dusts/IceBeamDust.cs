af19e3a5a073f294fe8cf4d9fa4cfeaacb898966
ghasttear1 <git@johntboyer.com>
2022-04-09 20:47:31 +1000

    Verified dusts are all present

---
+++ Dusts/IceBeamDust.cs
@@ -0,0 +1,30 @@
+﻿using AvalonTesting.Buffs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AvalonTesting.Dusts;
+
+public class IceBeamDust : ModDust
+{
+    public override bool Update(Dust dust)
+    {
+        foreach (NPC n in Main.npc)
+        {
+            if (n.getRect().Intersects(new Rectangle((int)dust.position.X, (int)dust.position.Y, 8, 8)))
+            {
+                n.AddBuff(ModContent.BuffType<IcySlowdown>(), 60 * 10);
+            }
+        }
+
+        float lightFade = dust.scale > 1 ? 1 : dust.scale;
+        Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), 0.1f * lightFade,
+            0.1f * lightFade, 0.1f * lightFade);
+        return true;
+    }
+
+    public override Color? GetAlpha(Dust dust, Color lightColor)
+    {
+        return new Color(255, 255, 255, 100);
+    }
+}
