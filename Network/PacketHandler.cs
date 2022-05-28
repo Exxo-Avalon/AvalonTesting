6cd51c377189a4e04c1021c012b4ff0bba7d006b
ghasttear1 <git@johntboyer.com>
2022-05-28 11:48:00 +1000

    PacketHandler.cs added netmode check to ensure that a packet is never sent on singleplayer.

--- Network/PacketHandler.cs
+++ Network/PacketHandler.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using Terraria;
 using Terraria.ID;
@@ -5,10 +6,25 @@ using Terraria.ModLoader;
 
 namespace AvalonTesting.Network;
 
-public abstract class PacketHandler<T> : IPacketHandler, ILoadable where T : NetworkArgs
+public abstract class PacketHandler<T> : IPacketHandler, ILoadable
+    where T : NetworkArgs
 {
     public int MessageId { get; private set; }
-    protected Mod Mod { get; private set; }
+    [NotNull]
+    protected Mod? Mod { get; private set; }
+
+    public void Send(T args, int toClient = -1, int ignoreClient = -1)
+    {
+        if (Main.netMode == NetmodeID.SinglePlayer)
+        {
+            return;
+        }
+
+        ModPacket packet = Mod.GetPacket();
+        packet.Write((byte)MessageId);
+        Send(packet, args);
+        packet.Send(toClient, ignoreClient);
+    }
 
     public void Load(Mod mod)
     {
@@ -31,14 +47,6 @@ public abstract class PacketHandler<T> : IPacketHandler, ILoadable where T : Net
     }
 
     protected abstract T Handle(BinaryReader reader);
-
-    public void Send(T args, int toClient = -1, int ignoreClient = -1)
-    {
-        ModPacket packet = Mod.GetPacket();
-        packet.Write((byte)MessageId);
-        Send(packet, args);
-        packet.Send(toClient, ignoreClient);
-    }
 
     protected abstract void Send(ModPacket packet, T args);
 }
