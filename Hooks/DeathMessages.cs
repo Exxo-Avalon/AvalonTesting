9783fcac788a29197cd31e0c5e6b96629343265d
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2022-07-29 01:02:15 +0500

    fporgr

--- Hooks/DeathMessages.cs
+++ Hooks/DeathMessages.cs
@@ -1,4 +1,7 @@
+using System;
 using Avalon.Common;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -8,272 +11,166 @@ namespace Avalon.Hooks;
 [Autoload(Side = ModSide.Both)]
 public class DeathMessages : ModHook
 {
-    protected override void Apply() => On.Terraria.Lang.CreateDeathMessage += OnCreateDeathMessage;
+    protected override void Apply() => IL.Terraria.Lang.CreateDeathMessage += BlahBald;
 
-    private static NetworkText OnCreateDeathMessage(
-        On.Terraria.Lang.orig_CreateDeathMessage orig, string deadPlayerName, int plr = -1, int npc = -1, int proj = -1,
-        int other = -1, int projType = 0, int plrItemType = 0)
+    private void BlahBald(ILContext il)
     {
-        NetworkText networkText = NetworkText.Empty;
-        NetworkText networkText2 = NetworkText.Empty;
-        NetworkText networkText3 = NetworkText.Empty;
-        NetworkText networkText4 = NetworkText.Empty;
-        if (proj >= 0)
-        {
-            networkText = NetworkText.FromKey(Lang.GetProjectileName(projType).Key);
-        }
+        ILCursor c = new(il);
 
-        if (npc >= 0)
+        try
         {
-            networkText2 = Main.npc[npc].GetGivenOrTypeNetName();
-        }
+            // lazy to properly document... so uh,
 
-        if (plr >= 0 && plr < 255)
-        {
-            networkText3 = NetworkText.FromLiteral(Main.player[plr].name);
-        }
+            c.GotoNext(i => i.MatchLdstr("DeathTextGeneric"))
+                .GotoNext(i => i.MatchStloc(8));
 
-        if (plrItemType >= 0)
-        {
-            networkText4 = NetworkText.FromKey(Lang.GetItemName(plrItemType).Key);
-        }
-
-        bool flag = networkText != NetworkText.Empty;
-        bool flag2 = plr >= 0 && plr < 255;
-        bool flag3 = networkText2 != NetworkText.Empty;
-        NetworkText result = NetworkText.Empty;
-        NetworkText empty = NetworkText.Empty;
-        empty = NetworkText.FromKey(Language.RandomFromCategory("DeathTextGeneric").Key, deadPlayerName,
-            Main.worldName);
-        if (Main.rand.NextBool(4))
-        {
-            int msg = Main.rand.Next(4);
-            if (msg == 0)
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 8)
+                .Emit(OpCodes.Ldarg, 1);
+            c.EmitDelegate<Func<NetworkText, string, NetworkText>>((text, deadPlayerName) =>
             {
-                empty = NetworkText.FromLiteral(deadPlayerName + " was tickled to smithereens");
-            }
-
-            if (msg == 1)
-            {
-                empty = NetworkText.FromLiteral(deadPlayerName + " was reduced to a fine paste");
-            }
-
-            if (msg == 2)
-            {
-                empty = NetworkText.FromLiteral(deadPlayerName + " became a tombstone dispenser");
-            }
-
-            if (msg == 3)
-            {
-                empty = NetworkText.FromLiteral(deadPlayerName + ".exe has stopped working. Program was closed");
-            }
-        }
-
-        if (flag2)
-        {
-            result = NetworkText.FromKey("DeathSource.Player", empty, networkText3, flag ? networkText : networkText4);
-        }
-        else if (flag3)
-        {
-            result = NetworkText.FromKey("DeathSource.NPC", empty, networkText2);
-        }
-        else if (flag)
-        {
-            result = NetworkText.FromKey("DeathSource.Projectile", empty, networkText);
-        }
-        else
-        {
-            #region falling
-            if (other == 0)
-            {
-                switch (Main.rand.Next(9) + 1)
+                if (Main.rand.NextBool(4))
                 {
-                    case 1:
-                        result = NetworkText.FromKey("DeathText.Fell_1", deadPlayerName);
-                        break;
-                    case 2:
-                        result = NetworkText.FromKey("DeathText.Fell_2", deadPlayerName);
-                        break;
-                    case 3:
-                        result = NetworkText.FromLiteral(deadPlayerName +
-                                                         " made a satisfying SPLAT when they hit the ground.");
-                        break;
-                    case 4:
-                        result = NetworkText.FromLiteral(deadPlayerName + " took a leap of faith...");
-                        break;
-                    case 5:
-                        result = NetworkText.FromLiteral(deadPlayerName + ", gravity sucks, y'know?");
-                        break;
-                    case 6:
-                        result = NetworkText.FromLiteral(deadPlayerName + " dreamt of flying.");
-                        break;
-                    case 7:
-                        result = NetworkText.FromLiteral(deadPlayerName + " is literally six feet under.");
-                        break;
-                    case 8:
-                        result = NetworkText.FromLiteral(deadPlayerName + "'s parachute was just a backpack.");
-                        break;
-                    case 9:
-                        result = NetworkText.FromLiteral(deadPlayerName + " got splattered all over " +
-                                                         Main.worldName);
-                        break;
+                    return Main.rand.Next(4) switch
+                    {
+                        3 => NetworkText.FromLiteral($"{deadPlayerName}.exe has stopped working. Program was closed"),
+                        2 => NetworkText.FromLiteral($"{deadPlayerName} became a tombstone dispenser"),
+                        1 => NetworkText.FromLiteral($"{deadPlayerName} was reduced to a fine paste"),
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} was tickled to smithereens"),
+                    };
                 }
-            }
-            #endregion falling
-            #region drowing
-            else if (other == 1)
-            {
-                switch (Main.rand.Next(7) + 1)
-                {
-                    case 1:
-                        result = NetworkText.FromKey("DeathText.Drowned_1", deadPlayerName);
-                        break;
-                    case 2:
-                        result = NetworkText.FromKey("DeathText.Drowned_2", deadPlayerName);
-                        break;
-                    case 3:
-                        result = NetworkText.FromKey("DeathText.Drowned_3", deadPlayerName);
-                        break;
-                    case 4:
-                        result = NetworkText.FromKey("DeathText.Drowned_4", deadPlayerName);
-                        break;
-                    case 5:
-                        result = NetworkText.FromLiteral(deadPlayerName + " blub blub blub...");
-                        break;
-                    case 6:
-                        result = NetworkText.FromLiteral(deadPlayerName + " thought they could breathe water.");
-                        break;
-                    case 7:
-                        result = NetworkText.FromLiteral(deadPlayerName + " asphyxiated.");
-                        break;
-                }
-            }
-            #endregion drowing
-            #region lava
-            else if (other == 2)
-            {
-                switch (Main.rand.Next(7) + 1)
-                {
-                    case 1:
-                        result = NetworkText.FromKey("DeathText.Lava_1", deadPlayerName);
-                        break;
-                    case 2:
-                        result = NetworkText.FromKey("DeathText.Lava_2", deadPlayerName);
-                        break;
-                    case 3:
-                        result = NetworkText.FromKey("DeathText.Lava_3", deadPlayerName);
-                        break;
-                    case 4:
-                        result = NetworkText.FromKey("DeathText.Lava_4", deadPlayerName);
-                        break;
-                    case 5:
-                        result = NetworkText.FromLiteral(deadPlayerName + " took a bath in a lake of fire.");
-                        break;
-                    case 6:
-                        result = NetworkText.FromLiteral(deadPlayerName + " became a charcoal briquette.");
-                        break;
-                    case 7:
-                        result = NetworkText.FromLiteral(deadPlayerName + " is HOT HOT HOT.");
-                        break;
-                }
-            }
-            #endregion lava
-            #region petrification
-            else if (other == 5)
-            {
-                switch (Main.rand.Next(5) + 1)
-                {
-                    case 1:
-                        result = NetworkText.FromKey("DeathText.Petrified_1", deadPlayerName);
-                        break;
-                    case 2:
-                        result = NetworkText.FromKey("DeathText.Petrified_2", deadPlayerName);
-                        break;
-                    case 3:
-                        result = NetworkText.FromKey("DeathText.Petrified_3", deadPlayerName);
-                        break;
-                    case 4:
-                        result = NetworkText.FromKey("DeathText.Petrified_4", deadPlayerName);
-                        break;
-                    case 5:
-                        result = NetworkText.FromLiteral(deadPlayerName + " was stoned.");
-                        break;
-                }
-            }
-            #endregion petrification
-            #region electrocution
-            else if (other == 10)
-            {
-                switch (Main.rand.Next(3) + 1)
-                {
-                    case 1:
-                        result = NetworkText.FromKey("DeathText.Electrocuted", deadPlayerName);
-                        break;
-                    case 2:
-                        result = NetworkText.FromLiteral(deadPlayerName + " got zzzzapped.");
-                        break;
-                    case 3:
-                        result = NetworkText.FromLiteral(deadPlayerName + " had an electrifying personality.");
-                        break;
-                }
-            }
-            #endregion electrocution
-            else
-            {
-                switch (other)
-                {
-                    case 3:
-                        result = NetworkText.FromKey("DeathText.Default", empty);
-                        break;
-                    case 4:
-                        result = NetworkText.FromKey("DeathText.Slain", deadPlayerName);
-                        break;
-                    case 6:
-                        result = NetworkText.FromKey("DeathText.Stabbed", deadPlayerName);
-                        break;
-                    case 7:
-                        result = NetworkText.FromKey("DeathText.Suffocated", deadPlayerName);
-                        break;
-                    case 8:
-                        result = NetworkText.FromKey("DeathText.Burned", deadPlayerName);
-                        break;
-                    case 9:
-                        result = NetworkText.FromKey("DeathText.Poisoned", deadPlayerName);
-                        break;
-                    case 11:
-                        result = NetworkText.FromKey("DeathText.TriedToEscape", deadPlayerName);
-                        break;
-                    case 12:
-                        result = NetworkText.FromKey("DeathText.WasLicked", deadPlayerName);
-                        break;
-                    case 13:
-                        result = NetworkText.FromKey("DeathText.Teleport_1", deadPlayerName);
-                        break;
-                    case 14:
-                        result = NetworkText.FromKey("DeathText.Teleport_2_Male", deadPlayerName);
-                        break;
-                    case 15:
-                        result = NetworkText.FromKey("DeathText.Teleport_2_Female", deadPlayerName);
-                        break;
-                    case 16:
-                        result = NetworkText.FromKey("DeathText.Inferno", deadPlayerName);
-                        break;
-                    case 17:
-                        result = NetworkText.FromKey("DeathText.DiedInTheDark", deadPlayerName);
-                        break;
-                    case 18:
-                        result = NetworkText.FromKey("DeathText.Starved", deadPlayerName);
-                        break;
-                    case 254:
-                        result = NetworkText.Empty;
-                        break;
-                    case 255:
-                        result = NetworkText.FromKey("DeathText.Slain", deadPlayerName);
-                        break;
-                }
-            }
-        }
+                return text;
+            });
+            c.Emit(OpCodes.Stloc, 8)
+                .GotoNext(i => i.MatchLdstr("DeathText.Fell_"))
+                .GotoNext(i => i.MatchLdcI4(2));
 
-        return result;
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(() => 9);
+            c.GotoNext(i => i.MatchStloc(7));
+
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 7)
+                .Emit(OpCodes.Ldarg, 1)
+                .Emit(OpCodes.Ldloc, 9);
+            c.EmitDelegate<Func<NetworkText, string, int, NetworkText>>((text, deadPlayerName, num) =>
+            {
+                if (num > 2)
+                {
+                    return (num - 3) switch
+                    {
+                        6 => NetworkText.FromLiteral($"{deadPlayerName} got splattered all over {Main.worldName}"),
+                        5 => NetworkText.FromLiteral($"{deadPlayerName}'s parachute was just a backpack."),
+                        4 => NetworkText.FromLiteral($"{deadPlayerName} is literally six feet under."),
+                        3 => NetworkText.FromLiteral($"{deadPlayerName} dreamt of flying."),
+                        2 => NetworkText.FromLiteral($"{deadPlayerName}, gravity sucks, y'know?"),
+                        1 => NetworkText.FromLiteral($"{deadPlayerName} took a leap of faith..."),
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} made a satisfying SPLAT when they hit the ground."),
+                    };
+                }
+                return text;
+            });
+            c.Emit(OpCodes.Stloc, 7)
+                .GotoNext(i => i.MatchLdstr("DeathText.Drowned_"))
+                .GotoNext(i => i.MatchLdcI4(4));
+
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(() => 7);
+            c.GotoNext(i => i.MatchStloc(7));
+
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 7)
+                .Emit(OpCodes.Ldarg, 1)
+                .Emit(OpCodes.Ldloc, 9);
+            c.EmitDelegate<Func<NetworkText, string, int, NetworkText>>((text, deadPlayerName, num) =>
+            {
+                if (num > 4)
+                {
+                    return (num - 5) switch
+                    {
+                        2 => NetworkText.FromLiteral($"{deadPlayerName} blub blub blub..."),
+                        1 => NetworkText.FromLiteral($"{deadPlayerName} thought they could breathe water."),
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} asphyxiated."),
+                    };
+                }
+                return text;
+            });
+            c.Emit(OpCodes.Stloc, 7)
+                .GotoNext(i => i.MatchLdstr("DeathText.Lava_"))
+                .GotoNext(i => i.MatchLdcI4(4));
+
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(() => 7);
+            c.GotoNext(i => i.MatchStloc(7));
+
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 7)
+                .Emit(OpCodes.Ldarg, 1)
+                .Emit(OpCodes.Ldloc, 9);
+            c.EmitDelegate<Func<NetworkText, string, int, NetworkText>>((text, deadPlayerName, num) =>
+            {
+                if (num > 4)
+                {
+                    return (num - 5) switch
+                    {
+                        2 => NetworkText.FromLiteral($"{deadPlayerName} took a bath in a lake of fire."),
+                        1 => NetworkText.FromLiteral($"{deadPlayerName} became a charcoal briquette."),
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} is HOT HOT HOT."),
+                    };
+                }
+                return text;
+            });
+            c.Emit(OpCodes.Stloc, 7)
+                .GotoNext(i => i.MatchLdstr("DeathText.Petrified_"))
+                .GotoNext(i => i.MatchLdcI4(4));
+
+            c.Index++;
+            c.Emit(OpCodes.Pop);
+            c.EmitDelegate(() => 5);
+            c.GotoNext(i => i.MatchStloc(7));
+
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 7)
+                .Emit(OpCodes.Ldarg, 1)
+                .Emit(OpCodes.Ldloc, 9);
+            c.EmitDelegate<Func<NetworkText, string, int, NetworkText>>((text, deadPlayerName, num) =>
+            {
+                if (num > 4)
+                {
+                    return (num - 5) switch
+                    {
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} was stoned."),
+                    };
+                }
+                return text;
+            });
+            c.Emit(OpCodes.Stloc, 7)
+                .GotoNext(i => i.MatchLdstr("DeathText.Electrocuted"))
+                .GotoNext(i => i.MatchStloc(7));
+
+            c.Index++;
+            c.Emit(OpCodes.Ldloc, 7)
+                .Emit(OpCodes.Ldarg, 1);
+            c.EmitDelegate<Func<NetworkText, string, NetworkText>>((text, deadPlayerName) =>
+            {
+                int num = Main.rand.Next(3);
+                if (num != 0)
+                {
+                    return (num - 1) switch
+                    {
+                        1 => NetworkText.FromLiteral($"{deadPlayerName} had an electrifying personality."),
+                        _ => NetworkText.FromLiteral($"{deadPlayerName} got zzzzapped."),
+                    };
+                }
+                return text;
+            });
+        }
+        catch (Exception e)
+        {
+            Avalon.Mod.Logger.Error($"[Death Messages IL Error]\n{e.Message}\n{e.StackTrace}");
+        }
     }
 }
