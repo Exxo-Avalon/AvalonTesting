500287f339b6f656249df871d49d891c1204c900
blahblahbal <jabawakijadehawk@gmail.com>
2022-07-31 18:50:44 -0700

    Drone Swarm

---
+++ Items/Weapons/Magic/DroneSwarm.cs
@@ -0,0 +1,66 @@
+using Avalon.Items.Material;
+using Avalon.Items.Placeable.Bar;
+using Avalon.Items.Placeable.Tile;
+using Avalon.Items.Weapons.Melee;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Weapons.Magic;
+
+class DroneSwarm : ModItem
+{
+    private byte soundCounter;
+    public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Drone Swarm");
+        Tooltip.SetDefault("Casts a spread of three homing drones\n'Thousands of bright yellow...!'");
+        SacrificeTotal = 1;
+        Item.staff[Type] = true;
+    }
+    public override Vector2? HoldoutOffset() => new Vector2(-100, 0);
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.damage = 25;
+        Item.autoReuse = true;
+        Item.scale = 1.2f;
+        Item.shootSpeed = 12f;
+        Item.rare = ItemRarityID.Yellow;
+        Item.width = dims.Width;
+        Item.useTime = 8;
+        Item.knockBack = 4f;
+        Item.shoot = ModContent.ProjectileType<Projectiles.Magic.Drone>();
+        Item.DamageType = DamageClass.Magic;
+        Item.mana = 5;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.value = Item.sellPrice(gold: 5);
+        Item.useAnimation = 8;
+        Item.height = dims.Height;
+    }
+    public override Color? GetAlpha(Color lightColor)
+    {
+        return new Color(255, 255, 255, 255);
+    }
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        soundCounter++;
+        for (int num194 = 0; num194 < 3; num194++)
+        {
+            float num195 = velocity.X;
+            float num196 = velocity.Y;
+            num195 += Main.rand.Next(-40, 41) * 0.05f;
+            num196 += Main.rand.Next(-40, 41) * 0.05f;
+            Projectile.NewProjectile(source, position.X, position.Y, num195, num196, type, damage, knockback, player.whoAmI, 0f, 0f);
+        }
+        if (soundCounter > 3)
+        {
+            SoundEngine.PlaySound(SoundID.Item43, position);
+            soundCounter = 0;
+        }
+        return false;
+    }
+}
--- Projectiles/Magic/Drone.cs
+++ Projectiles/Magic/Drone.cs
@@ -3,57 +3,51 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Audio;
+using System.IO;
 
-namespace Avalon.Projectiles.Melee;
+namespace Avalon.Projectiles.Magic;
 
-public class BlahBeam : ModProjectile
+public class Drone : ModProjectile
 {
     private int tileCollideCounter;
     public bool readyToHome = true;
     public override void SetStaticDefaults()
     {
-        DisplayName.SetDefault("Blah Beam");
+        DisplayName.SetDefault("Drone");
     }
     public override void SetDefaults()
     {
-        Projectile.width = 16;
-        Projectile.height = 16;
-        Projectile.aiStyle = 27;
-        Projectile.DamageType = DamageClass.Melee;
+        Projectile.width = 12;
+        Projectile.height = 12;
+        Projectile.aiStyle = -1;
+        Projectile.DamageType = DamageClass.Magic;
         Projectile.penetrate = 5;
         Projectile.alpha = 255;
         Projectile.friendly = true;
         Projectile.usesLocalNPCImmunity = true;
-        Projectile.localNPCHitCooldown = 20;
+        Projectile.localNPCHitCooldown = 60;
     }
     public override Color? GetAlpha(Color lightColor)
     {
+        return Color.White;
         if (Projectile.localAI[1] >= 15f)
         {
             return new Color(255, 255, 255, Projectile.alpha);
         }
-        if (Projectile.localAI[1] < 5f)
-        {
-            return Color.Transparent;
-        }
         int num7 = (int)((Projectile.localAI[1] - 5f) / 10f * 255f);
         return new Color(num7, num7, num7, num7);
     }
-    public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+    public override void SendExtraAI(BinaryWriter writer)
     {
-        int randomNum = Main.rand.Next(7);
-        if (randomNum == 0) target.AddBuff(20, 300);
-        else if (randomNum == 1) target.AddBuff(24, 200);
-        else if (randomNum == 2) target.AddBuff(31, 120);
-        else if (randomNum == 3) target.AddBuff(39, 300);
-        else if (randomNum == 4) target.AddBuff(44, 300);
-        else if (randomNum == 5) target.AddBuff(70, 240);
-        else if (randomNum == 6) target.AddBuff(69, 300);
-        readyToHome = false;
+        writer.Write(tileCollideCounter);
+    }
+    public override void ReceiveExtraAI(BinaryReader reader)
+    {
+        tileCollideCounter = reader.ReadInt32();
     }
     public override bool OnTileCollide(Vector2 oldVelocity)
     {
-        if (Projectile.type == ModContent.ProjectileType<BlahBeam>())
+        if (Projectile.type == ModContent.ProjectileType<Drone>())
         {
             SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
             tileCollideCounter++;
@@ -77,17 +71,29 @@ public class BlahBeam : ModProjectile
         return false;
     }
     public float maxSpeed = 10f + Main.rand.NextFloat(10f);
-    public float homeDistance = 600;
+    public float homeDistance = 450;
     public float homeStrength = 5f;
     public float homeDelay;
     public override void AI()
     {
-        Lighting.AddLight(Projectile.position, 255 / 255f, 175 / 255f, 0);
-
+        Lighting.AddLight(Projectile.position, 219 / 255f, 205 / 255f, 79 / 255f);
+        //for (int num26 = 0; num26 < 4; num26++)
+        //{
+        //    float x2 = Projectile.position.X - Projectile.velocity.X / 10f * num26;
+        //    float y2 = Projectile.position.Y - Projectile.velocity.Y / 10f * num26;
+        //    int num27 = Dust.NewDust(new Vector2(x2 + 5, y2 + 5), 5, 5, ModContent.DustType<Dusts.ContagionSpray>(), 0f, 0f, 0, default, 1f);
+        //    Main.dust[num27].alpha = Projectile.alpha;
+        //    Main.dust[num27].color.A = 0;
+        //    Main.dust[num27].scale *= 1f;
+        //    Main.dust[num27].position = new Vector2(x2, y2);
+        //    Main.dust[num27].velocity *= 0f;
+        //    Main.dust[num27].noGravity = true;
+        //}
+        Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
         if (!readyToHome)
         {
             homeDelay++;
-            if(homeDelay >= 20)
+            if(homeDelay >= 30)
             {
                 readyToHome = true;
                 homeDelay = 0;
