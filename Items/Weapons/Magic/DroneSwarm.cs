37fdd9100e4fe700562e07fa889a2c1500dbabc4
blahblahbal <jabawakijadehawk@gmail.com>
2022-08-01 11:14:01 -0700

    Update DroneSwarm.cs

--- Items/Weapons/Magic/DroneSwarm.cs
+++ Items/Weapons/Magic/DroneSwarm.cs
@@ -1,10 +1,5 @@
-using Avalon.Items.Material;
-using Avalon.Items.Placeable.Bar;
-using Avalon.Items.Placeable.Tile;
-using Avalon.Items.Weapons.Melee;
 using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -13,7 +8,6 @@ namespace Avalon.Items.Weapons.Magic;
 
 class DroneSwarm : ModItem
 {
-    private byte soundCounter;
     public override void SetStaticDefaults()
     {
         DisplayName.SetDefault("Drone Swarm");
@@ -31,14 +25,16 @@ class DroneSwarm : ModItem
         Item.shootSpeed = 12f;
         Item.rare = ItemRarityID.Yellow;
         Item.width = dims.Width;
-        Item.useTime = 8;
+        Item.useTime = 4;
         Item.knockBack = 4f;
+        Item.UseSound = SoundID.Item8;
+        Item.reuseDelay = 12;
         Item.shoot = ModContent.ProjectileType<Projectiles.Magic.Drone>();
         Item.DamageType = DamageClass.Magic;
         Item.mana = 5;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.value = Item.sellPrice(gold: 5);
-        Item.useAnimation = 8;
+        Item.useAnimation = 12;
         Item.height = dims.Height;
     }
     public override Color? GetAlpha(Color lightColor)
@@ -47,19 +43,14 @@ class DroneSwarm : ModItem
     }
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
-        soundCounter++;
+        Vector2 pos = player.Center + new Vector2(30, 0).RotatedBy(player.AngleTo(Main.MouseWorld));
         for (int num194 = 0; num194 < 3; num194++)
         {
             float num195 = velocity.X;
             float num196 = velocity.Y;
             num195 += Main.rand.Next(-40, 41) * 0.05f;
             num196 += Main.rand.Next(-40, 41) * 0.05f;
-            Projectile.NewProjectile(source, position.X, position.Y, num195, num196, type, damage, knockback, player.whoAmI, 0f, 0f);
-        }
-        if (soundCounter > 3)
-        {
-            SoundEngine.PlaySound(SoundID.Item43, position);
-            soundCounter = 0;
+            Projectile.NewProjectile(source, pos.X, pos.Y, num195, num196, type, damage, knockback, player.whoAmI, 0f, 0f);
         }
         return false;
     }
