a994a63a61baeec119e1d8ad2fca3ded014176d2
TerrorPenguin <terra_penguin@live.com>
2022-08-04 19:31:16 +1000

    Purple Dungeon Brick wip

---
+++ Items/Placeable/Tile/PurpleBrick.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Placeable.Tile;
+
+class PurpleBrick : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Purple Brick");
+        SacrificeTotal = 100;
+    }
+
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.autoReuse = true;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<Tiles.PurpleBrick>();
+        Item.width = dims.Width;
+        Item.useTurn = true;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.maxStack = 999;
+        Item.useAnimation = 15;
+        Item.height = dims.Height;
+    }
+    /* public override void AddRecipes()
+    {
+        CreateRecipe(1).AddIngredient(ModContent.ItemType<Wall.PurpleBrickWall>(), 4).AddTile(TileID.WorkBenches).Register();
+        CreateRecipe(1).AddIngredient(ModContent.ItemType<Wall.PurpleSlabWall>(), 4).AddTile(TileID.WorkBenches).Register();
+        CreateRecipe(1).AddIngredient(ModContent.ItemType<Wall.PurpleTiledWall>(), 4).AddTile(TileID.WorkBenches).Register();
+    } */
+}
