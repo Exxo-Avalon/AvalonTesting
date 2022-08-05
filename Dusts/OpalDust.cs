a83ffdd4847225df0e676793bbdfac31a4a7ffc5
Jacob Jacobsphere <geometricwarrior@gmail.com>
2022-08-05 09:05:47 -0500

    Update OpalDust.cs

--- Dusts/OpalDust.cs
+++ Dusts/OpalDust.cs
@@ -8,7 +8,7 @@ public class OpalDust : ModDust
 {
     public override bool Update(Dust dust)
     {
-        Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), Main.DiscoR / 255, Main.DiscoG / 255, Main.DiscoB / 255);
+        Lighting.AddLight((int)(dust.position.X / 16f), (int)(dust.position.Y / 16f), Main.DiscoR / 255f, Main.DiscoG / 255f, Main.DiscoB / 255f);
         return true;
     }
     public override Color? GetAlpha(Dust dust, Color lightColor)
