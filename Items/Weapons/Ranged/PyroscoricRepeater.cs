0208cbe218cb9fab7a62e209ce272fd3c3dc0334
blahblahbal <jabawakijadehawk@gmail.com>
2022-08-05 18:43:24 -0700

    Pyrotritan

--- Items/Weapons/Ranged/PyroscoricRepeater.cs
+++ Items/Weapons/Ranged/PyroscoricRepeater.cs
@@ -70,6 +70,7 @@ class PyroscoricRepeater : ModItem
     }
     public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
     {
+        /* CODE TO FORCE PLAYER TO LOOK IN DIRECTION YOU FIRE FOR 3 SHOTS
         // check if the first shot
         if (HowManyTimesHasTheFunnyArrowsBeenShotPleaseTellMeItsImportant == 1)
         {
@@ -90,7 +91,7 @@ class PyroscoricRepeater : ModItem
             {
                 position += muzzleOffset;
             }
-        }
+        }*/
         if (type == ProjectileID.WoodenArrowFriendly)
         {
             type = ModContent.ProjectileType<Projectiles.Ranged.PyroBolt>();
