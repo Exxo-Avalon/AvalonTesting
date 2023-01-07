868b195f8aaa5d694c8a1bec7bf0b0692c18f80c
GreenTea0617 <jonahrichman617@gmail.com>
2023-01-07 15:25:19 -0500

    Waypoint

---
+++ Systems/WPMirrorSystem.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+namespace Avalon.Systems{
+    public class WPSystem : ModSystem{
+
+    }
+}
